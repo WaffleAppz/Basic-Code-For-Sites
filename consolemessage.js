362dbf84855ab9bfd638c39e78642788647c67b4
WaffleAppz <93115264+WaffleAppz@users.noreply.github.com>
2022-05-04 19:40:37 +1200

    Delete consolemessage.js

--- consolemessage.js
+++
@@ -1,6 +0,0 @@
-// used for telling the user messages via console
-
-console.log("Hello user this was made using SnapHap on github")
-console.log("DO NOT paste anything in the console")
-console.log("Thanks. The SnapHap team")
-// below you can use the code console.log("your text here") for stuff to show up in the console
